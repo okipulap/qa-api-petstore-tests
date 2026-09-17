2f9b6030de813a67a09dfba8fd1c7ecfc8ad2668
okipulap <cdeadg@yandex.ru>
2026-09-17 15:38:01 +0300

    E2E test for User

--- src/test/java/apiTests/scenarios/UserScenario.java
+++ src/test/java/apiTests/scenarios/UserScenario.java
@@ -6,7 +6,11 @@ import apiTests.models.user.User;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.*;
+import static apiTests.asserts.UserAssertions.assertUserFieldsMatch;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Epic("Petstore API: Пользовательские сценарии")
 @Owner("Nikita Tkachenko")
@@ -18,20 +22,41 @@ public class UserScenario {
 		client = new UserClient();
 	}
 
-//	@Test
-//	@Tag("Positive")
-//	@Tag("E2E")
-//	@DisplayName("E2E-сценарий: жизненный цикл пользователя")
-//	@Feature("Пользователь")
-//	void userScenario() {
-//		//Создание пользователя
-//		User userPostRequest = UserFactory.randomUser();
-//		User userPostResponse = client.postUser(userPostRequest);
-//
-//		//Получение пользователя
-//		User userGetResponse =
-//			client.getUserByUsername(userPostResponse.getUsername());
-//
-//		User userPutRequest = UserFactory.updateUser(userPostResponse.getId(), userPostResponse.getUsername())
-//	}
+	@Test
+	@Tag("Positive")
+	@Tag("E2E")
+	@DisplayName("E2E-сценарий: жизненный цикл пользователя")
+	@Feature("Пользователь")
+	void userScenario() {
+		// Создание пользователя
+		User userPostRequest = UserFactory.randomUser();
+		User userPostResponse = client.postUser(userPostRequest);
+		assertUserFieldsMatch(userPostRequest, userPostResponse);
+
+		// Получение пользователя
+		User userGetResponse =
+			client.getUserByUsername(userPostResponse.getUsername());
+		assertUserFieldsMatch(userPostRequest, userGetResponse);
+
+		// Изменение пользователя
+		User userPutRequest =
+			UserFactory.updateUser(userPostResponse.getId(), userPostResponse.getUsername());
+		User userPutResponse = client.putUserByUsername(userPutRequest, userGetResponse.getUsername());
+		assertUserFieldsMatch(userPutRequest, userPutResponse);
+
+		// Получение после изменения
+		User userGetAfterUpdateResponse =
+			client.getUserByUsername(userPutResponse.getUsername());
+		assertUserFieldsMatch(userPutRequest, userGetAfterUpdateResponse);
+
+		// Удаление пользователя
+		Response userDeleteResponse =
+			client.deleteUser(userGetAfterUpdateResponse.getUsername());
+		assertEquals(HttpStatus.SC_OK, userDeleteResponse.getStatusCode());
+
+		// Получение пользователя после удаления
+		Response getAfterDeleteResponse =
+			client.getUserExpected404(userGetAfterUpdateResponse.getUsername());
+		assertEquals(HttpStatus.SC_NOT_FOUND, getAfterDeleteResponse.getStatusCode());
+	}
 }
